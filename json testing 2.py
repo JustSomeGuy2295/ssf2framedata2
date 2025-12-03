db8be5c7ca8c870613ea6b6f0e5763262961176f
JustSomeGuy2295 <jesasx775@gmail.com>
2025-12-03 11:56:45 +1000

    Reworked character json formatting
    - Able to extract all the info needed for discord embeds

--- json testing 2.py
+++ json testing 2.py
@@ -3,13 +3,25 @@ import json
 char = 'Bandana Dee'
 move = 'Jab'
 
+with open('data/characters.json', 'r') as c:
+    charidentifier = json.load(c)
+
 with open(f'data/info/{char}.json', 'r') as f:
     charinfo = json.load(f)
-    
-for i, j in enumerate(charinfo[move]):
+
+# Character info for embed title, author, etc
+for idx, identifier in enumerate(charidentifier[char]):
+    print(f'{identifier} {charidentifier[char][identifier]}')
+                        # charidentifier[char][identifier] returns characters id, color, icon
+
+# Move info for embed description    
+for i, hit in enumerate(charinfo[move]["Hitboxes"]):
     # Iterates through the different hits the move has
-    print(f'Hit: {j}')
+    print(f'Hit: {hit}')
     
     # For every value listed in each hit prints the value name and value value
-    for idx, info in enumerate(charinfo[move][f'{j}']):
-        print(f'{info}: {charinfo[move][f'{j}'][f'{info}']}')
+    for idx, info in enumerate(charinfo[move]["Hitboxes"][f'{hit}']):
+        print(f'{info}: {charinfo[move]["Hitboxes"][f'{hit}'][f'{info}']}')
+    
+    print(f'Image: {charinfo[move]["Images"]["Full Speed"][f"{hit}"]}')
+    print(f'Slowmo: {charinfo[move]["Images"]["Slowmo"][f"{hit}"]}')
